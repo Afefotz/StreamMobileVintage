edcf2d8728985756acdd3444281828da77661e0d
Alfredo Berumen Mendez <108704017+Afefotz@users.noreply.github.com>
2026-03-09 13:44:36 -0600

    Titles for room creator

--- index.js
+++ index.js
@@ -42,7 +42,18 @@ document.getElementById("roomName").addEventListener("keypress", function (e) {
 // Función para cambiar el tema en tiempo real en el Index
 function cambiarTemaPreview() {
   const selectedTheme = document.getElementById("start-theme").value;
+  //Cambia Colores y diseño
   document.body.className = selectedTheme;
+  // 2. Cambiar el texto del título de la ventana
+const indexTitle = document.getElementById('index-title');
+            
+if (selectedTheme === 'theme-win95') indexTitle.innerText = 'ScoreVS_Creator.exe';
+else if (selectedTheme === 'theme-modern') indexTitle.innerText = 'Crear Nueva Sala';
+else if (selectedTheme === 'theme-modern-light') indexTitle.innerText = 'Crear Nueva Sala';
+else if (selectedTheme === 'theme-neon') indexTitle.innerText = 'SYS_INIT // ROOM_GEN';
+else if (selectedTheme === 'theme-pastel') indexTitle.innerText = '✧ Match Creator ✧';
+else if (selectedTheme === 'theme-stone') indexTitle.innerText = 'Forge New Arena';
+else if (selectedTheme === 'theme-laser') indexTitle.innerText = 'INIT_LASER_GRID';
 }
 
 // Ejecutar la vista previa apenas cargue la página
